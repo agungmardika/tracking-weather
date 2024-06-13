3bfdc17f97b63f50f65d193b2eeb86692395af79
agungmardika <agungdmardika@gmail.com>
2024-06-14 02:08:10 +0800

    alhamdulillah done

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// const searchButton = document.querySelector("#button-addon2");
-// const inputKeyword = document.querySelector("#input-keyword");
+const searchButton = document.querySelector("#button-addon2");
+const inputKeyword = document.querySelector("#input-keyword");
 
-// searchButton.addEventListener("click", function () {
-//   fetch(
-    // "https://api.openweathermap.org/data/2.5/weather?q=" +
-    //   inputKeyword.value +
-    //   "&appid=7589d0b96634ec6c634eca41b16808ca&units=metric"
-//   )
-//     .then((response) => response.json())
-//     .then((response) => {
-//       let result = document.querySelector(".result");
+searchButton.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      inputKeyword.value +
+      "&appid=7589d0b96634ec6c634eca41b16808ca&units=metric"
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      let result = document.querySelector(".result");
 
-//       result.innerHTML = `<h2 style="margin-bottom: 15px;">${response.name}, ${response.sys.country}</h2>
-//                                 <h5><span class="temp">${response.main.temp}°С</span> <span class="temp">${response.weather[0].description}</span></h5>
-//                                 <p style="margin-bottom: 17px;">Temperature from ${response.main.temp_min}°С to ${response.main.temp_max}°С</p>
-//                                 <h5>Wind Speed : ${response.wind.speed} m/s</h5>
-                   
-//                                 <h5 style="margin-bottom: 17px;">Clouds : ${response.clouds.all}%</h5>
-//                                 <h4 style="color: #012443;">Geo Coordinates : [${response.coord.lat}, ${response.coord.lon}]</h4>`;
-//     });
-//   inputKeyword.value = null;
-// });
+      result.innerHTML = `<h2 class="text-xl font-semibold mb-2" id="location">${response.name}, ${response.sys.country}</h2>
+                <p class="text-lg mb-1"><span class="temp text-2xl">${response.main.temp}°С</span> <span class="condition">${response.weather[0].description}</span></p>
+                <p class="mb-2">Temperature from <span id="temp-min">${response.main.temp_min}°С</span> to <span id="temp-max">${response.main.temp_max}°С</span></p>
+                <p class="mb-1">Wind Speed: <span id="wind-speed">${response.wind.speed}m/s</span></p>
+                <p class="mb-1">Clouds: <span id="clouds">${response.clouds.all}%</span></p`;
+    });
+  inputKeyword.value = null;
+});
